f093b806d3c951be768d2de1f4c2ca4bf0ea1748
Toni Matkovic <tmatkovic@optimizerx.com>
2020-10-15 12:03:06 +0200

    Added labels & minor cleanup

--- src/views/Pet.vue
+++ src/views/Pet.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    {{ pet().name }}
+    <h1>{{ animal.name }} ({{ $route.params.species }})</h1>
+    <p>Age: {{ animal.age }} years old</p>
+    <p>Breed: {{ animal.breed }}</p>
   </div>
 </template>
 
@@ -10,7 +12,9 @@ import { mapState } from 'vuex'
 export default {
   name: 'Pet',
   data () {
-    return {}
+    return {
+      animal: {}
+    }
   },
   computed: {
     // cats () { return this.$store.state.cats }, -> identical as ...mapState(['cats']), but a bad practice
@@ -19,11 +23,9 @@ export default {
       'dogs'
     ])
   },
-  methods: {
-    pet () {
-      // this.cats is equal as doing this[cats], like on next line
-      return this[this.$route.params.species][this.$route.params.id]
-    }
+  mounted () {
+    // this.cats is equal as doing this[cats], like on next line
+    this.animal = this[this.$route.params.species][this.$route.params.id]
   }
 }
 </script>
