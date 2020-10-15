f48403fdd828d2d3589dfa8719032869a888718f
Toni Matkovic <tmatkovic@optimizerx.com>
2020-10-15 11:58:59 +0200

    Using route params

--- src/views/Pet.vue
+++ src/views/Pet.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
-    Pet
+    {{ pet().name }}
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
-  name: 'Pet'
+  name: 'Pet',
+  data () {
+    return {}
+  },
+  computed: {
+    // cats () { return this.$store.state.cats }, -> identical as ...mapState(['cats']), but a bad practice
+    ...mapState([
+      'cats',
+      'dogs'
+    ])
+  },
+  methods: {
+    pet () {
+      // this.cats is equal as doing this[cats], like on next line
+      return this[this.$route.params.species][this.$route.params.id]
+    }
+  }
 }
 </script>
 
